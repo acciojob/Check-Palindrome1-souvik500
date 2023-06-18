e548e5b90c4a707ccd6b6e617bc94f0194f9c3b3
souvik500 <120314952+souvik500@users.noreply.github.com>
2023-06-18 14:08:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,8 +5,12 @@ function palindrome(str){
 	const reversedString = str.split('').reverse().join('');
     if(str === reversedString)
           return true;
-    
+    else
           return false;
 	
 }
+
+let res = palindrome("Racecar")
+console.log(res);
+
 module.exports = palindrome
